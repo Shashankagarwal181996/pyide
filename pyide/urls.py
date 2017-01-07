5cd17ac2f309c29ef7107ec75b2c73d5ce08d8d9
Shashank <shashankagarwal181996@gmail.com>
2017-01-08 00:19:49 +0530

    fixing static for js files on heroku

--- pyide/urls.py
+++ pyide/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from pyide import views
+from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+from pyide import views
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
@@ -27,3 +29,4 @@ urlpatterns = [
 
 ]
 urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
